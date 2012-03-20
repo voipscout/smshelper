387be34b1887939f35b26567423bf575e9df8350
Voip Scout <voipscout@gmail.com>
2012-03-20 13:57:03 +0100

    docs

--- lib/smshelper/message.rb
+++ lib/smshelper/message.rb
@@ -10,22 +10,16 @@ module Smshelper
       is_gsm(@text) ? (@utf_8 = false) : (@utf_8 = true)
     end
 
-    # TODO: Id the language with both APIs, fallback to charset != gsm if api isn't available
     def is_gsm(text)
       dl = Smshelper::Languagetools::Charset.instance
       lang = dl.is_gsm(text)
     end
-    # THIS IS ANOTHER TEST
-    # @classic shit
-    def to_hex
-      @text = @text.unpack('U*').collect {|x| sprintf '%02X', x}.join
-    end
 
-    # Converts the object into textual markup given a specific format.
-    #
-    # @param [Symbol] format the format type, `:text` or `:html`
-    # @return [String] the object converted into the expected format.
-    # AND EVEN MORE CLASS
+    # def to_hex
+    #   @text = @text.unpack('U*').collect {|x| sprintf '%02X', x}.join
+    # end
+
+    # convert text to UCS-2 BigEndian
     def to_hex_be
       @text = @text.unpack('U*').collect {|x| sprintf '%04X', x}.join
     end
