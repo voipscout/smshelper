b15a4741ff9e20c2b5c6fd1f5c3e034ed4e5319e
Voip Scout <voipscout@gmail.com>
2012-05-25 23:52:15 +0200

    nexmo wipping

--- lib/smshelper/api/mycoolsms.rb
+++ lib/smshelper/api/mycoolsms.rb
@@ -11,10 +11,10 @@ module Smshelper
 
       def send_message(message)
         options = {
-        :number => message.recipient,
-        :message => message.text,
-        :senderid => message.sender,
-        :function => 'sendSms'}
+          :number => message.recipient,
+          :message => message.text,
+          :senderid => message.sender,
+          :function => 'sendSms'}
         options = options.merge(@extra_options) unless @extra_options.nil?
         resp = JSON.parse(post 'api-socket.php', :extra_query => options)
         process_response_code(resp) ? (@sent_message_ids << resp['smsid']; resp['smsid']) : (raise ErrorDuringSend, resp)
@@ -28,6 +28,16 @@ module Smshelper
         JSON.parse(post 'api-socket.php', :extra_query => {:function => 'doHlrLookup', :number => number})
       end
 
+      def get_callback_response(args = {})
+        DeliveryReport.new(
+                           :message_id => args['message_id'],
+                           :timestamp => Time.now,
+                           :delivered => ((args['status'] == '1') ? true : false),
+                           :original_params => args
+                           )
+      end
+
+      private
       def process_response_code(code)
         code['success']
       end
